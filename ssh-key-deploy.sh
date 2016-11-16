de9c0fef42f4faebe58db0a41d502e7fd12da87b
Gavin Beeton <h8r1dx4@gmail.com>
2016-11-16 19:40:24 +0200

    Original Commit

---
+++ ssh-key-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+date=`date +%F`/
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+list=$dir'router.list'
+filelines=`cat $list`
+
+echo `date`": ""SSH Key Deploy Start" >> $dir/log.txt
+echo Start
+
+for line in $filelines ; do
+	echo $line
+	scp ~/.ssh/id_dsa.pub backups@$line:/
+	VAR1=$?
+
+	wait 10
+
+	ssh backups@$line /user ssh-keys import user=backups public-key-file=id_dsa.pub
+	VAR2=$?
+	echo "Router :" $line "Key Deploy Finished at :" `date` "Key Copy :" $VAR1 "Key Install :" $VAR2 >> $dir/log.txt
+done
+
+echo `date`": ""SSH Key Deploy End" >> $dir/log.txt
